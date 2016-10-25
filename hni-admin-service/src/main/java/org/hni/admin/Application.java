4cf9d3d888ffd822614fbb19124949d9e510b2a4
Jeff Parker <jeff3parker@gmail.com>
2016-10-25 17:50:16 -0500

    add swagger config

--- hni-admin-service/src/main/java/org/hni/admin/Application.java
+++ hni-admin-service/src/main/java/org/hni/admin/Application.java
@@ -6,12 +6,26 @@ import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.hni.admin.filter.ResponseCorsFilter;
 
+import io.swagger.jaxrs.config.BeanConfig;
+
 @ApplicationPath("api") /* this is the servlet mapping */
 public class Application extends ResourceConfig {
 
     public Application() {
-        packages("org.hni.admin");
+        packages("org.hni");
         register(JacksonFeature.class);
         register(ResponseCorsFilter.class);
+        
+        register(io.swagger.jaxrs.listing.ApiListingResource.class);
+        register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
+
+        BeanConfig beanConfig = new BeanConfig();
+        beanConfig.setVersion("1.5.0");
+        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setHost("localhost:8080");
+        beanConfig.setBasePath("/service/api");
+        beanConfig.setResourcePackage("org.hni");
+        beanConfig.setScan(true);
+     
     }
 }
