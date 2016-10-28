6141957ac0c301016be046f40151bfb8c7736c81
Jeff Parker <jeff3parker@gmail.com>
2016-10-28 10:48:31 -0500

    add spring files and externalize swagger config

--- hni-admin-service/src/main/java/org/hni/admin/Application.java
+++ hni-admin-service/src/main/java/org/hni/admin/Application.java
@@ -1,17 +1,27 @@
 package org.hni.admin;
 
+import java.util.Properties;
+
 import javax.ws.rs.ApplicationPath;
 
 import org.apache.commons.lang.StringUtils;
 import org.glassfish.jersey.jackson.JacksonFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.hni.admin.filter.ResponseCorsFilter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.context.ContextLoader;
 
 import io.swagger.jaxrs.config.BeanConfig;
 
 @ApplicationPath("api/v1") /* this is the servlet mapping */
 public class Application extends ResourceConfig {
 
+	private static final String DEFAULT_HOST = "http://localhost:8080";
+	private static final String DEFAULT_VERSION = "1.5.0";
+	private static final String DEFAULT_BASEPATH = "/hni-admin-service/api/v1";
+	private static final String DEFAULT_SCHEME = "http";
+	private static final String DEFAULT_RESOURCE_PACKAGE = "org.hni";
+	
     public Application() {
         packages("org.hni");
         register(JacksonFeature.class);
@@ -20,13 +30,20 @@ public class Application extends ResourceConfig {
         register(io.swagger.jaxrs.listing.ApiListingResource.class);
         register(io.swagger.jaxrs.listing.SwaggerSerializers.class);
 
-        String host = String.format("localhost:%s", StringUtils.defaultIfBlank(System.getProperty("port"), "8080"));
+        ApplicationContext rootCtx = ContextLoader.getCurrentWebApplicationContext();
+        Properties myConfiguration = (Properties) rootCtx.getBean("hniProperties");
+        
+        String host = StringUtils.defaultIfBlank(myConfiguration.getProperty("swagger.host"), DEFAULT_HOST);
+        String version = StringUtils.defaultIfBlank(myConfiguration.getProperty("swagger.version"), DEFAULT_VERSION);
+        String basePath = StringUtils.defaultIfBlank(myConfiguration.getProperty("swagger.basePath"), DEFAULT_BASEPATH);
+        String scheme = StringUtils.defaultIfBlank(myConfiguration.getProperty("swagger.scheme"), DEFAULT_SCHEME);
+ 
         BeanConfig beanConfig = new BeanConfig();
-        beanConfig.setVersion("1.5.0");
-        beanConfig.setSchemes(new String[]{"http"});
+        beanConfig.setVersion(version);
+        beanConfig.setSchemes(new String[]{scheme});
         beanConfig.setHost(host);
-        beanConfig.setBasePath("/hni-admin/api/v1");
-        beanConfig.setResourcePackage("org.hni");
+        beanConfig.setBasePath(basePath);
+        beanConfig.setResourcePackage(DEFAULT_RESOURCE_PACKAGE);
         beanConfig.setScan(true);
      
     }
