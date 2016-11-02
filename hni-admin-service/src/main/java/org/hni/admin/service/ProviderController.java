116da378ab99f3b93dff4d15c2b01d6bc18b0067
Jeff Parker <jeff3parker@gmail.com>
2016-11-02 11:11:01 -0500

    add crud for providerLocations

--- hni-admin-service/src/main/java/org/hni/admin/service/ProviderController.java
+++ hni-admin-service/src/main/java/org/hni/admin/service/ProviderController.java
@@ -15,7 +15,7 @@ import javax.ws.rs.core.MediaType;
 
 import org.hni.provider.om.Provider;
 import org.hni.provider.om.ProviderLocation;
-import org.hni.provider.service.MenuService;
+import org.hni.provider.service.ProviderLocationService;
 import org.hni.provider.service.ProviderService;
 import org.hni.user.dao.AddressDAO;
 import org.hni.user.om.Address;
@@ -33,7 +33,7 @@ public class ProviderController {
 	private static final Logger logger = LoggerFactory.getLogger(ProviderController.class);
 	
 	@Inject private ProviderService providerService;
-	@Inject private MenuService menuService;
+	@Inject private ProviderLocationService providerLocationService;
 	@Inject private AddressDAO addressDao;
 	
 	@GET
@@ -114,8 +114,7 @@ public class ProviderController {
 	, response = ProviderLocation.class
 	, responseContainer = "")
 	public Collection<ProviderLocation> getProviderLocations(@PathParam("id") Long id) {
-		// TODO
-		return Collections.emptyList();
+		return providerLocationService.with(new Provider(id));
 	}
 	
 	@POST
@@ -127,8 +126,8 @@ public class ProviderController {
 	, response = ProviderLocation.class
 	, responseContainer = "")
 	public ProviderLocation addProviderLocation(@PathParam("id") Long id, ProviderLocation providerLocation) {
-		// TODO
-		return null;
+		providerLocation.setProvider(new Provider(id));
+		return providerLocationService.save(providerLocation);
 	}
 
 	@DELETE
@@ -139,7 +138,11 @@ public class ProviderController {
 	, response = ProviderLocation.class
 	, responseContainer = "")
 	public ProviderLocation addProviderLocation(@PathParam("id") Long id, @PathParam("plid") Long plid) {
-		// TODO
+		ProviderLocation providerLocation = providerLocationService.get(plid);
+		if (providerLocation.getProvider().getId().equals(id)) {
+			providerLocationService.delete(new ProviderLocation(plid));
+		}
+		//TODO: throw error?
 		return null;
 	}
 	
@@ -150,14 +153,14 @@ public class ProviderController {
 		, notes = "Use the /addresses API to update addresses"
 		, response = Provider.class
 		, responseContainer = "")
-	public Provider addAddressToProviderLocation(@PathParam("id") Long id, Address address) {
-		//TODO
-		Provider provider = providerService.get(id);
-		if (null != provider) {
-			provider.getAddresses().add(address);
-			providerService.save(provider);
+	public ProviderLocation addAddressToProviderLocation(@PathParam("id") Long id, @PathParam("plid") Long plid, Address address) {
+		ProviderLocation providerLocation = providerLocationService.get(plid);
+		if (providerLocation.getProvider().getId().equals(id)) {
+			providerLocation.getAddresses().add(address);
+			providerLocationService.save(providerLocation);
 		}
-		return provider;
+
+		return providerLocation;
 	}
 
 	@DELETE
@@ -167,16 +170,16 @@ public class ProviderController {
 		, notes = ""
 		, response = Provider.class
 		, responseContainer = "")
-	public Provider removeAddressFromProviderLocation(@PathParam("id") Long id, @PathParam("addressId") Long addressId) {
-		// TODO
-		Provider provider = providerService.get(id);
-		if (null != provider) {
+	public ProviderLocation removeAddressFromProviderLocation(@PathParam("id") Long id, @PathParam("plid") Long plid, @PathParam("addressId") Long addressId) {
+		ProviderLocation providerLocation = providerLocationService.get(plid);
+		if (providerLocation.getProvider().getId().equals(id)) {
 			Address address = addressDao.get(addressId);
 			if ( null != address ) {
-				provider.getAddresses().remove(address); // Hibernate will manage the mapping table for us.
-				providerService.save(provider);
-			}
+				providerLocation.getAddresses().remove(address); // Hibernate will manage the mapping table for us.
+				providerLocationService.save(providerLocation);
+			}			
 		}
-		return provider;
+
+		return providerLocation;
 	}	
 }
