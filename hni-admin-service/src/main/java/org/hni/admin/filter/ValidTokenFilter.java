3c70e64d6128d99d7a33497a6b052072c66099fa
a0barth <a0barth@walmart.com>
2016-11-07 09:01:42 -0600

    changed to Inject from default constructor, still unable to test

--- hni-admin-service/src/main/java/org/hni/admin/filter/ValidTokenFilter.java
+++ hni-admin-service/src/main/java/org/hni/admin/filter/ValidTokenFilter.java
@@ -1,24 +1,19 @@
 package org.hni.admin.filter;
 
+import javax.inject.Inject;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.shiro.web.filter.authz.AuthorizationFilter;
-import org.hni.security.service.DefaultUserSecurityService;
 import org.hni.security.service.UserSecurityService;
 import org.hni.user.om.User;
 
-
 public class ValidTokenFilter extends AuthorizationFilter {
 	private static final String TOKEN_HEADER = "X-hni-token";
-	private static final UserSecurityService userSecurityService = new DefaultUserSecurityService();
 
-	/*
-	 * TODO: may be able to get away with an
-	 * 
-	 * @Inject UserSecurityService userSecurityService
-	 */
+	@Inject
+	private static UserSecurityService userSecurityService;
 
 	@Override
 	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
