5dcf4365a196f5310fafb88400a644592392f6b0
Christopher Hughes <Christopher.Hughes@walmart.com>
2016-11-08 14:11:31 -0500

    Fixed unit tests.

--- hni-order/src/test/java/org/hni/order/service/TestOrderService.java
+++ hni-order/src/test/java/org/hni/order/service/TestOrderService.java
@@ -23,7 +23,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations={"classpath:test-applicationContext.xml"} )
-@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 @Transactional
 public class TestOrderService {
 	
@@ -55,21 +55,20 @@ public class TestOrderService {
 
 	@Test
 	public void testOrderComplete() {
-		Order order = OrderTestData.getTestOrder();
+		Date pickupDate = new Date();
 
+		Order order = OrderTestData.getTestOrder();
 		// Checks that the pickup date is null prior to the completion methods
 		assertEquals(null, order.getPickupDate());
 
-		Date pickupDate = new Date();
 		Order returnOrder = orderService.complete(order);
-
 		// Verifies that the pickup date has been updated
-		assertTrue(returnOrder.getPickupDate().after(pickupDate));
+		assertTrue(pickupDate.before(returnOrder.getPickupDate()));
 
 		//Checks that it was properly loaded into database
 		Order orderFromDatabase = orderService.get(order.getId());
 		assertNotNull(orderFromDatabase);
-		assertTrue(orderFromDatabase.getPickupDate().after(pickupDate));
+		assertTrue(pickupDate.before(orderFromDatabase.getPickupDate()));
 	}
 
 	@Test
