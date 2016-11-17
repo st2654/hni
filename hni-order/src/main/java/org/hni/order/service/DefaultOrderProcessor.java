43221a3ef174aa717332768241dcda177a32bccb
acresp2 <Anthony.Crespo@walmart.com>
2016-11-17 17:58:01 -0500

    A bit more fleshed out orderProcessor

--- hni-order/src/main/java/org/hni/order/service/DefaultOrderProcessor.java
+++ hni-order/src/main/java/org/hni/order/service/DefaultOrderProcessor.java
@@ -2,6 +2,7 @@ package org.hni.order.service;
 
 import org.hni.order.om.PartialOrder;
 import org.hni.order.om.TransactionPhase;
+import org.hni.provider.om.MenuItem;
 import org.hni.provider.om.ProviderLocation;
 import org.hni.provider.service.GeoCodingService;
 import org.hni.provider.service.MenuService;
@@ -10,9 +11,9 @@ import org.hni.user.om.Address;
 import org.hni.user.om.User;
 
 import javax.inject.Inject;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class DefaultOrderProcessor implements OrderProcessor {
     private static final double PROVIDER_SEARCH_RADIUS = 5;
@@ -45,21 +46,50 @@ public class DefaultOrderProcessor implements OrderProcessor {
                 if (address.isPresent()) {
                     List<ProviderLocation> nearbyProviders = geoService.searchNearbyLocations(address.get(), PROVIDER_SEARCH_RADIUS);
                     order.setProviderLocationsForSelection(nearbyProviders);
+                    List<MenuItem> items = new ArrayList<>();
+                    for (ProviderLocation location : nearbyProviders) {
+                        //TODO get the currently available menu items, not just first
+                        items.add(menuService.with(location.getProvider()).iterator().next().getMenuItems().iterator().next());
+                    }
+                    order.setMenuItemsForSelection(items);
+                    for (int i = 0 ; i < 3; i++) {
+                        output += i + "." + nearbyProviders.get(i).getName() + "(" + items.get(i).getName() + ")";
+                    }
+                    order.setTransactionPhase(TransactionPhase.CHOOSING_LOCATION);
+                } else {
+                    output = "Invalid address, please try again";
                 }
                 break;
             case CHOOSING_LOCATION:
                 //below may need to get moved in order to fit the format
                 //Subway (Ham Sandwich), Taco Bell (Chicken Soft Tacos),  Panera (Steak Sandwich)
-                ProviderLocation location = order.getProviderLocationsForSelection().get(Integer.parseInt(message));
-                order.setChosenProvider(location);
-                order.setMenuItemsForSelection(menuService.with(location.getProvider()).stream()
-                        .flatMap(menu -> menu.getMenuItems().stream())
-                        .collect(Collectors.toList()));
+                try {
+                    int index = Integer.parseInt(message);
+                    if (index < 1 || index > 3) {
+                        throw new IndexOutOfBoundsException();
+                    }
+                    ProviderLocation location = order.getProviderLocationsForSelection().get(index-1);
+                    order.setChosenProvider(location);
+                    order.setTransactionPhase(TransactionPhase.CONFIRM_OR_CONTINUE);
+                } catch (NumberFormatException | IndexOutOfBoundsException e) {
+                    output = "Please provide a number between 1-3";
+                }
                 break;
             case CHOOSING_MENU_ITEM:
                 //this is chosen w/ provider now I believe
                 break;
             case CONFIRM_OR_CONTINUE:
+                switch (message.toUpperCase()) {
+                    case "CONFIRM":
+                        //TODO create new completed order
+                        break;
+                    case "CONTINUE":
+                        //TODO mark the partial order as having ometihng ordered
+                        order.setTransactionPhase(TransactionPhase.CHOOSING_LOCATION);
+                        break;
+                    default:
+                        output = "Please respond with CONFIRM or CONTINUE";
+                }
                 break;
             default:
                 //shouldn't get here
