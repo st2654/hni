8f3d3917a4bd25216e66088c718328f8c6e4b603
yichaoyu <yichaoyu@gmail.com>
2016-11-17 17:55:39 -0500

    update workflow logic as a keyword can interrupt an existing workflow add e2e test for EventServiceFactory

--- hni-events/src/main/java/org/hni/events/service/EventServiceFactory.java
+++ hni-events/src/main/java/org/hni/events/service/EventServiceFactory.java
@@ -29,16 +29,26 @@ public class EventServiceFactory {
     }
 
     public String handleEvent(final Event event) {
-        final SessionState state = sessionStateDao.get(event.getSessionId());
-        final EventName eventName;
-        if (state == null) {
-            eventName = parseKeyWordToEventName(event.getTextMessage());
+        final String sessionId = event.getSessionId();
+        final SessionState state = sessionStateDao.get(sessionId);
+        EventName eventName = parseKeyWordToEventName(event.getTextMessage());
+        if (eventName == null) {
+            if (state == null) {
+                // not a keyword, nor having a in progress workflow
+                return "Unknown keyword " + event.getTextMessage();
+            } else {
+                eventName = state.getEventName();
+            }
+        } else {
+            if (state != null) {
+                // clear previous workflow as a new keyword is received
+                sessionStateDao.delete(sessionId);
+            }
             if (!sessionStateDao.insert(new SessionState(eventName, event.getSessionId(), event.getPhoneNumber()))) {
                 throw new RuntimeException("Insert failed. Maybe the state is inserted by others. Retry or Reset");
             }
-        } else {
-            eventName = state.getEventName();
         }
+        // set value to current workflow's value when it is not a keyword value
         return eventServiceMap.get(eventName).handleEvent(event);
     }
 
@@ -57,7 +67,7 @@ public class EventServiceFactory {
             case "DONATE":
                 return EventName.DONATE;
             default: // No valid keyword entered
-                throw new RuntimeException("Not understand the keyword. Retry");
+                return null;
         }
     }
 }
--- hni-events/src/main/java/org/hni/events/service/om/EventState.java
+++ hni-events/src/main/java/org/hni/events/service/om/EventState.java
@@ -11,18 +11,14 @@ public enum EventState {
     STATE_REGISTER_GET_EMAIL(102, EventName.REGISTER),
     STATE_REGISTER_CONFIRM_EMAIL(103, EventName.REGISTER),
     STATE_REGISTER_GET_AUTH_CODE(104, EventName.REGISTER),
-    STATE_REGISTER_COMPLETE(105, EventName.REGISTER);
+    STATE_REGISTER_MORE_AUTH_CODES(105, EventName.REGISTER);
 
     private static final Map<EventName, EventState> INITIAL_STATES;
-    private static final Map<EventName, EventState> END_STATES;
     private static final Map<Integer, EventState> ALL_STATES;
 
     static {
         INITIAL_STATES = new HashMap<>();
         INITIAL_STATES.put(EventName.REGISTER, STATE_REGISTER_START);
-
-        END_STATES = new HashMap<>();
-        END_STATES.put(EventName.REGISTER, STATE_REGISTER_COMPLETE);
 
         ALL_STATES = new HashMap<>();
         Arrays.stream(values()).forEach(s -> ALL_STATES.put(s.getStateCode(), s));
