3f0d45ce1d0a7aaab352e8f2d1c190859a6d8b43
Madhavi Bhat <Madhavi.Bhat@walmart.com>
2016-11-18 00:19:01 -0500

    code for getproviderlocation by cust id address

--- hni-admin-service/src/main/java/org/hni/admin/service/ProviderController.java
+++ hni-admin-service/src/main/java/org/hni/admin/service/ProviderController.java
@@ -1,14 +1,8 @@
 package org.hni.admin.service;
 
-import java.util.Collection;
-import java.util.Collections;
-
-import javax.inject.Inject;
-import javax.validation.constraints.NotNull;
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-
-import org.apache.commons.lang.StringUtils;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.StringUtils;
 import org.hni.provider.om.Provider;
 import org.hni.provider.om.ProviderLocation;
 import org.hni.provider.service.ProviderLocationService;
@@ -19,8 +13,19 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import javax.inject.Inject;
+import javax.validation.constraints.NotNull;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import java.util.Collection;
 
 @Api(value = "/providers", description = "Operations on Providers and ProviderLocations")
 @Component
