2e9208205351c56ad669ea52f15e539a38d8c535
Wayne Menezes <Wayne.Menezes@walmart.com>
2016-11-19 11:24:51 -0500

    fixed maven multi module pom issues

--- hni-organization/src/test/resources/hsql/h2-test-data.sql
+++ hni-organization/src/test/resources/hsql/h2-test-data.sql
@@ -1,7 +1,7 @@
-insert into users values(1, 'Super','User','M','mphone', 'superuser@hni.com', 0, 'qqIDI/cPs/CqdMo15uDhwAN/Zc+Z9VOUjLNGgxlC864=', '42M7kr4oektqA6Jgy9u1YQ==', now(), '0');
-insert into users values(2, 'Freddy','Fikes','M','479-555-1212', '', 0, '', '', now(), '0');
-insert into users values(3, 'Mikey','Multiphone','M','479-555-4321', '', 0, '', '', now(), '0');
-insert into users values(4, 'Mikey','Multiphone','M','479-555-5678', '', 0, '', '', now(), '0');
+insert into users values('Super','User','M','mphone', 'superuser@hni.com', 0, 'qqIDI/cPs/CqdMo15uDhwAN/Zc+Z9VOUjLNGgxlC864=', '42M7kr4oektqA6Jgy9u1YQ==', now(), '0');
+insert into users values(2,'Freddy','Fikes','M','479-555-1212', '', 0, '', '', now(), '0');
+insert into users values(3,'Mikey','Multiphone','M','479-555-4321', '', 0, '', '', now(), '0');
+insert into users values(4,'Mikey','Multiphone','M','479-555-5678', '', 0, '', '', now(), '0');
 insert into users values(5, 'Ericka', 'Energy', 'F', '123-456-7830', '' ,0, '', '', now(), '0');
 insert into users values(6, 'Barbara', 'Bollingsworth', 'F', '123-456-7830', '' ,0, '', '', now(), '0');
 
