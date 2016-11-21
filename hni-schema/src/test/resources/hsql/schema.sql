6623dd60a410a2cb7a589300a6c25bdfb5c16c3c
Jeff Parker <jeff3parker@gmail.com>
2016-11-21 10:10:33 -0600

    resolve schema/data issues

---
+++ hni-schema/src/test/resources/hsql/schema.sql
@@ -0,0 +1,267 @@
+SET MODE MySQL;
+-- MySQL Workbench Forward Engineering
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Table `users`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NULL,
+  `last_name` VARCHAR(255) NULL,
+  `gender_code` VARCHAR(1) NULL,
+  `mobile_phone` VARCHAR(45) NULL,
+  `email` VARCHAR(255) NULL,
+  `deleted` INT NULL,
+  `hashed_secret` VARCHAR(255) NULL,
+  `salt` VARCHAR(255) NULL,
+  `created` DATETIME NULL,
+  `opt_out` INT NULL DEFAULT 0 COMMENT 'true/false whether the user is opt-in/out.  Default ‘0’',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `organizations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `organizations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `phone` VARCHAR(45) NULL,
+  `website` VARCHAR(255) NULL,
+  `logo` VARCHAR(255) NULL,
+  `created` DATETIME NOT NULL,
+  `created_by` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `security_roles`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `security_roles` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_organization_role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user_organization_role` (
+  `user_id` INT NOT NULL,
+  `organization_id` INT NOT NULL,
+  `role_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `organization_id`, `role_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `providers`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `providers` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `address_id` INT NULL,
+  `created` DATETIME NOT NULL,
+  `created_by` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `provider_locations`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `provider_locations` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `provider_id` INT NOT NULL,
+  `address_id` INT NULL,
+  `created` DATETIME NOT NULL,
+  `created_by` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `orders`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `orders` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `user_id` INT NOT NULL,
+  `provider_location_id` INT NOT NULL,
+  `order_date` DATETIME NOT NULL,
+  `ready_date` DATETIME NULL,
+  `pickup_date` DATETIME NULL,
+  `subtotal` DECIMAL(10,2) NULL,
+  `tax` DECIMAL(10,2) NULL,
+  `created_by` INT NULL COMMENT 'surrogate to users',
+  `status_id` INT NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `menus`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `menus` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(45) NULL,
+  `provider_id` INT NOT NULL,
+  `start_hour` INT NULL COMMENT 'starting hour item is available in 24hr',
+  `end_hour` INT NULL COMMENT 'ending hour item is available in 24hr',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `menu_items`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `menu_items` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `menu_id` INT NOT NULL,
+  `name` VARCHAR(255) NULL,
+  `description` TEXT NULL,
+  `price` DECIMAL(10,2) NULL,
+  `expires` DATETIME NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `order_items`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_items` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `order_id` INT NOT NULL,
+  `menu_item_id` INT NOT NULL,
+  `quantity` INT NOT NULL,
+  `amount` DECIMAL(10,2) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `addresses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `addresses` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(255) NULL,
+  `address_line1` VARCHAR(255) NULL,
+  `address_line2` VARCHAR(255) NULL,
+  `city` VARCHAR(45) NULL,
+  `state` VARCHAR(2) NULL,
+  `zip` VARCHAR(15) NULL,
+  `longitude` VARCHAR(45) NULL,
+  `latitude` VARCHAR(45) NULL,
+  `timezone` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `activation_codes`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `activation_codes` (
+  `activation_code` VARCHAR(255) NOT NULL,
+  `organization_id` INT NOT NULL,
+  `meals_authorized` INT NULL,
+  `meals_remaining` INT NULL,
+  `activated` TINYINT NULL COMMENT 'true/false whether this voucher can be used',
+  `comments` VARCHAR(255) NULL,
+  `created` VARCHAR(45) NULL,
+  `user_id` INT NULL COMMENT 'the user who “owns” this voucher',
+  PRIMARY KEY (`activation_code`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `user_provider_role`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `user_provider_role` (
+  `user_id` INT NOT NULL,
+  `provider_id` INT NOT NULL,
+  `role_id` INT NOT NULL,
+  PRIMARY KEY (`user_id`, `provider_id`, `role_id`))
+ENGINE = InnoDB;
+
+
+
+-- -----------------------------------------------------
+-- Table `organization_addresses`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `organization_addresses` (
+  `organization_id` INT NOT NULL,
+  `address_id` INT NOT NULL,
+  PRIMARY KEY (`organization_id`, `address_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `provider_location_hours`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `provider_location_hours` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `provider_location_id` INT NOT NULL,
+  `dow` VARCHAR(3) NULL,
+  `open_hour` INT NULL COMMENT 'open hour in 24hr',
+  `close_hour` INT NULL COMMENT 'close hour in 24hr',
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `security_permissions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `security_permissions` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `domain` VARCHAR(45) NULL,
+  `value` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `security_role_permissions`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `security_role_permissions` (
+  `role_id` INT NOT NULL,
+  `permission_id` INT NOT NULL,
+  `all_instances` INT NULL DEFAULT 0,
+  PRIMARY KEY (`role_id`, `permission_id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `payment_instruments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `payment_instruments` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `provider_id` INT NULL,
+  `card_type` VARCHAR(45) NULL,
+  `card_number` VARCHAR(45) NULL COMMENT 'hashed value',
+  `status` VARCHAR(45) NULL COMMENT 'activated or not',
+  `orginal_balance` DECIMAL(10,2) NULL,
+  `balance` DECIMAL(10,2) NULL,
+  `last_used_datetime` DATETIME NULL,
+  `pin_number` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `order_payments`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `order_payments` (
+  `order_id` INT NOT NULL,
+  `payment_instrument_id` INT NOT NULL,
+  `amount` DECIMAL(10,2) NULL,
+  `created_by` INT NOT NULL,
+  `created_date` DATETIME NOT NULL,
+  PRIMARY KEY (`order_id`, `payment_instrument_id`))
+ENGINE = InnoDB;
+
---
+++ hni-schema/src/test/resources/hsql/test-data.sql
@@ -0,0 +1,108 @@
+insert into users values(1,'Super','User','M','mphone', 'superuser@hni.com', 0, 'qqIDI/cPs/CqdMo15uDhwAN/Zc+Z9VOUjLNGgxlC864=', '42M7kr4oektqA6Jgy9u1YQ==', now(), '0');
+insert into users values(2,'Freddy','Fikes','M','479-555-1212', '', 0, '', '', now(), '0');
+insert into users values(3,'Mikey','Multiphone','M','479-555-4321', '', 0, '', '', now(), '0');
+insert into users values(4,'Mikey','Multiphone','M','479-555-5678', '', 0, '', '', now(), '0');
+insert into users values(5, 'Ericka', 'Energy', 'F', '123-456-7830', '' ,0, '', '', now(), '0');
+insert into users values(6, 'Barbara', 'Bollingsworth', 'F', '123-456-7830', '' ,0, '', '', now(), '0');
+
+insert into organizations values(1, 'Not Impossible', 'phone', 'website', 'logo', now(), 1);
+insert into organizations values(2, 'Samaritan House', 'phone', 'website', 'logo', now(), 1);
+insert into organizations values(3, 'Volunteer Organization', 'phone', 'website', 'logo', now(), 1);
+
+insert into user_organization_role values(1, 2, 1);
+insert into user_organization_role values(2, 2, 7);
+insert into user_organization_role values(3, 2, 7);
+insert into user_organization_role values(4, 2, 7);
+insert into user_organization_role values(5, 4, 5);
+insert into user_organization_role values(5, 4, 2);
+insert into user_organization_role values(5, 2, 5);
+insert into user_organization_role values(5, 2, 2);
+insert into user_organization_role values(6, 4, 5);
+insert into user_organization_role values(6, 4, 2);
+insert into user_organization_role values(6, 2, 5);
+insert into user_organization_role values(6, 2, 2);
+
+insert into menus values(1, '"Subway Lunch', 1, 10, 16); /* 10am to 4pm */
+insert into menu_items values(1, 1, 'Ham Sandwich', 'ham and cheese with LTP', 6.99, null);
+insert into menu_items values(2, 1, 'Turkey Sandwich', 'turkey and cheese with LTP', 7.99, null);
+insert into menu_items values(3, 1, 'Roast beef Sandwich', 'beef and cheese with LTP', 8.99, null);
+insert into menu_items values(4, 1, 'Club Sandwich', 'turkey, ham and cheese with LTP', 7.95, null);
+
+insert into orders values(1, 2, 1, dateadd('DAY', -2, current_date), dateadd('DAY', -2, current_date), null, 9.95, 1.20, 1, 1);
+insert into orders values(2, 2, 1, dateadd('DAY', -2, current_date), dateadd('DAY', -2, current_date), null, 9.95, 1.20, 1, 1);
+insert into orders values(3, 2, 1, dateadd('DAY', -2, current_date), dateadd('DAY', -2, current_date), null, 9.95, 1.20, 1, 1);
+
+insert into security_roles values(1,'Super User');
+insert into security_roles values(2,'Administrator');
+insert into security_roles values(3,'Volunteer');
+insert into security_roles values(4,'Client');
+insert into security_roles values(5,'User');
+
+insert into security_permissions values(1,'*','*'); /* user-user only! */
+
+insert into security_permissions values(11,'organizations','create');
+insert into security_permissions values(12,'organizations','read');
+insert into security_permissions values(13,'organizations','update');
+insert into security_permissions values(14,'organizations','delete');
+insert into security_permissions values(15,'organizations','*');
+
+insert into security_permissions values(21,'users','create');
+insert into security_permissions values(22,'users','read');
+insert into security_permissions values(23,'users','update');
+insert into security_permissions values(24,'users','delete');
+insert into security_permissions values(25,'users','*');
+
+insert into security_permissions values(41,'providers','create');
+insert into security_permissions values(42,'providers','read');
+insert into security_permissions values(43,'providers','update');
+insert into security_permissions values(44,'providers','delete');
+insert into security_permissions values(45,'providers','*');
+
+insert into security_permissions values(61,'orders','create');
+insert into security_permissions values(62,'orders','read');
+insert into security_permissions values(63,'orders','update');
+insert into security_permissions values(64,'orders','delete');
+insert into security_permissions values(65,'orders','*');
+insert into security_permissions values(67,'orders','provision');
+
+
+insert into security_permissions values(81,'activation-codes','create');
+insert into security_permissions values(82,'activation-codes','read');
+insert into security_permissions values(83,'activation-codes','update');
+insert into security_permissions values(84,'activation-codes','delete');
+insert into security_permissions values(85,'activation-codes','*');
+
+/* super-user / admin */
+insert into security_role_permissions values(1,1,1);
+insert into security_role_permissions values(2,15,0);
+insert into security_role_permissions values(2,25,1);
+insert into security_role_permissions values(2,45,1);
+insert into security_role_permissions values(2,65,1);
+insert into security_role_permissions values(2,85,1);
+
+/* volunteer */
+insert into security_role_permissions values(3,12,0);
+insert into security_role_permissions values(3,67,1);
+
+/* client/user */
+insert into security_role_permissions values(4,12,0);
+insert into security_role_permissions values(5,12,0);
+
+insert into activation_codes values('1234567890', 2, 10, 10, 1, null, now(), null);
+insert into activation_codes values('7h-1234567890', 2, 10, 10, 2, 'freddy has activated this', now(), 2);
+
+insert into addresses values (1, 'subway corp addr', '1251 Phoenician way', '', 'columbus','oh','43240', '-82.98402279999999', '40.138686','etc');
+insert into addresses values (2, 'kfc corp addr', '10790 parkridge blvd', '', 'reston','va','20191', '-77.316008', '38.9455603','etc');
+insert into addresses values (3, 'wendys corp addr', '1251 Phoenician way', '', 'columbus','oh','43240', '-82.98402279999999', '40.138686','etc');
+insert into addresses values (4, 'subway #1 addr', '1251 Phoenician way', '', 'columbus','oh','43240', '-82.98402279999999', '40.138686','etc');
+insert into addresses values (5, 'kfc #1 addr', '10790 parkridge blvd', '', 'reston','va','20191', '-77.316008', '38.9455603','etc');
+insert into addresses values (6, 'wendys #1 addr', '1251 Phoenician way', '', 'columbus','oh','43240', '-82.98402279999999', '40.138686','etc');
+
+insert into providers values(1, 'Subway', 1, now(), 1);
+insert into providers values(2, 'KFC', 2, now(), 1);
+insert into providers values(3, 'Wendys', 3, now(), 1);
+
+insert into provider_locations values(1, 'Subway #1', 1, 4, now(), 1);
+insert into provider_locations values(2, 'KFC #1', 2, 5, now(), 1);
+insert into provider_locations values(3, 'Wendy #1', 3, 6, now(), 1);
+
