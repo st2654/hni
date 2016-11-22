99a9fb71279736ad48b24e16a0ef9a857c3cb3a6
Jeff Parker <jeff3parker@gmail.com>
2016-11-22 12:20:03 -0600

    add support for unlocking orders, return Order+User with limited view of user

--- hni-order/src/main/java/org/hni/order/service/LockingService.java
+++ hni-order/src/main/java/org/hni/order/service/LockingService.java
@@ -3,6 +3,7 @@ package org.hni.order.service;
 public interface LockingService {
 
 	boolean acquireLock(String key);
+	boolean acquireLock(String key, Long ttlMinutes);
 	boolean isLocked(String key);
 	void releaseLock(String key);
 	void shutdown();
